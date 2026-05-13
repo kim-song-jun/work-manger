4c8f367464e6dce5d9e42e542d4586c9e18c4b1e
kimsongjun <kinsongjun13@gmail.com>
2026-05-13 22:51:33 +0900

    chore: remove old openapi-generator cruft from git tracking

--- apps/mobile/lib/api/openapi/test/company_subscription_status_enum_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:test/test.dart';
-import 'package:wm_api/wm_api.dart';
-
-// tests for CompanySubscriptionStatusEnum
-void main() {
-
-  group(CompanySubscriptionStatusEnum, () {
-  });
-}
--- apps/mobile/lib/api/openapi/test/invoice_status_enum_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:test/test.dart';
-import 'package:wm_api/wm_api.dart';
-
-// tests for InvoiceStatusEnum
-void main() {
-
-  group(InvoiceStatusEnum, () {
-  });
-}
